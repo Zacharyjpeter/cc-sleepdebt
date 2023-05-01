c32afbcda07fda90dae45d909cfc041920f07ffc
Zacharyjpeter <zacharyjpeter@gmail.com>
2023-04-30 19:58:19 -0500

    created actual sleep function

--- app.js
+++ app.js
@@ -5,8 +5,26 @@ const getSleepHours = (day) => {
     day === "tuesday" ||
     day === "wednesday" ||
     day === "thursday" ||
-    day === "friday"
+    day === "friday" ||
+    day === "saturday" ||
+    day === "sunday"
   ) {
     return 8;
   }
 };
+
+//output test
+console.log(getSleepHours("monday"));
+
+function getActualSleepHours() {
+  getSleepHours("monday") +
+    getSleepHours("tuesday") +
+    getSleepHours("wednesday") +
+    getSleepHours("thursday") +
+    getSleepHours("friday") +
+    getSleepHours("saturday") +
+    getSleepHours("sunday");
+}
+
+//output test
+console.log(getActualSleepHours());
